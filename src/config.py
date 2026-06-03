99b518944cc6c8e1af1314cd5e87526bdd3b2639
TheBarrelExploit <andres.ramosl@cun.edu.co>
2026-06-02 23:01:14 -0500

    fix: add default value bucket_name

--- src/config.py
+++ src/config.py
@@ -12,7 +12,7 @@ class Settings(BaseSettings):
     r2_account_id: str = ""
     r2_access_key_id: str = ""
     r2_secret_access_key: str = ""
-    r2_bucket_name: str = ""
+    r2_bucket_name: str = "chicago-crimes-analytics"
     r2_endpoint_url: str = ""
 
     # DasgHub / MLflow
