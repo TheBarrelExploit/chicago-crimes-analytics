088b2cc89057ea0ce4f24e42053c4a388ed3668a
TheBarrelExploit <andres.ramosl@cun.edu.co>
2026-07-09 18:31:47 -0500

    feat(stubs): add __init__.pyi to define stub package

---
+++ stubs/streamlit_option_menu/__init__.pyi
@@ -0,0 +1,14 @@
+from typing import Any
+
+def option_menu(
+    menu_title: Any,
+    options: Any,
+    default_index: int = ...,
+    menu_icon: Any = ...,
+    icons: Any = ...,
+    orientation: str = ...,
+    styles: Any = ...,
+    manual_select: Any = ...,
+    key: Any = ...,
+    on_change: Any = ...,
+) -> Any: ...
